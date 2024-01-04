91a77d9c88f2e0b66fa340b15c7cfc5fe526933f
jon_prog <jonashenri.aquino@gmail.com>
2024-01-04 18:35:38 -0300

    add:auth front-end

--- frontend/main.js
+++ frontend/main.js
@@ -1,4 +1,10 @@
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
-import './assets/css/style.css';
 
+import Login from './modules/Login'
+
+const login = new Login('.form-login');
+const register = new Login('.form-register');
+
+login.init();
+register.init();
---
+++ frontend/modules/Login.js
@@ -0,0 +1,43 @@
+const validator = require('validator');
+
+export default class Login{
+	constructor(formClass){
+		this.form = document.querySelector(formClass);
+	};
+
+	init(){
+		this.events();
+	}
+	
+	events(){
+		if(!this.form) return;
+		this.form.addEventListener('submit', e => {
+			e.preventDefault();
+			this.validate(e);
+		});
+	}
+
+	validate(e){
+		const el = e.target;
+		const emailInput = el.querySelector('input[name="email"]');
+		const passwordInput = el.querySelector('input[name="password"]');
+
+		let emailError = el.querySelector('.emailError');
+		let passwordError = el.querySelector('.passwordError');
+		let error = false;
+		
+		if(!validator.isEmail(emailInput.value)){
+			emailError.style.display = 'block';
+			error = true;
+		}
+
+		if(passwordInput.value.length < 4 || passwordInput.value.length > 50 ){
+			passwordError.style.display = 'block';
+			error = true;
+		}
+		console.log(error)
+		if(!error) return el.submit();
+	}
+};
+
+
