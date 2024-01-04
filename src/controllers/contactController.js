fc5ac49956e4fe142c7d3f37873d92add1667371
jon_prog <jonashenri.aquino@gmail.com>
2024-01-04 09:10:49 -0300

    add:edit contact

--- src/controllers/contactController.js
+++ src/controllers/contactController.js
@@ -34,7 +34,22 @@ exports.contactValues = async function(req,res){
 };
 
 exports.editContact = async function(req,res){
-    if(!req.params.id) return res.render('404');
-    const contact = await Contact.searchId(req.params.id);
+    try{
+        if(!req.params.id) return res.render('404');
+        const contact = new Contact(req.body);
+        await contact.edit(req.params.id);
     
+        if(contact.errors.length > 0){
+            req.flash('errors',contact.errors);
+            req.session.save(()=>res.redirect('back'));
+            return;
+        }
+    
+        req.flash('success','Contato editado com sucesso!')
+        req.session.save(()=>res.redirect(`/contact/${contact.contact._id}`));
+        return;  
+    }catch(e){
+        console.log(e);
+        return res.render('404')
+    }
 }
