77f708535e52bcc94be17b00cf4923c70a2c267a
jon_prog <jonashenri.aquino@gmail.com>
2024-01-04 00:00:13 -0300

    update:middlewares

--- src/middlewares/middleware.js
+++ src/middlewares/middleware.js
@@ -16,3 +16,12 @@ exports.csrfMiddleware = (req,res,next) => {
     res.locals.csrfToken = req.csrfToken();
     next();
 }
+
+exports.loginRequired = (req,res,next) => {
+    if(!req.session.user){
+        req.flash('errors','VocÊ precisa fazer login')
+        req.session.save(()=>res.redirect('/'));
+        return;
+    }
+    next();
+}
