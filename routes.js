b5e142ee981f1ca368e0789c6fc68ca355cef61c
jon_prog <jonashenri.aquino@gmail.com>
2024-01-03 23:12:46 -0300

    update:routes

--- routes.js
+++ routes.js
@@ -2,6 +2,7 @@ const express = require('express');
 const route = express.Router();
 const homeController = require('./src/controllers/homeController');
 const loginController = require('./src/controllers/loginController');
+const contactController = require('./src/controllers/conactController');
 
 //route home
 route.get('/',homeController.index);
@@ -11,5 +12,8 @@ route.get('/login',loginController.index);
 route.post('/login/register',loginController.register);
 route.post('/login/logon',loginController.logon);
 route.get('/login/logout',loginController.logout);
+
+//routes contact
+route.get('/contact',contactController.index)
 
 module.exports = route;
